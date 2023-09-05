0b7cac2b9ef9861c5ee4bef3286aa53620cc329d
larafabricio <132107051+larafabricio@users.noreply.github.com>
2023-09-05 15:22:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const apiButton = document.getElementById('apiButton');
+const apiData = document.getElementById('apiData')
+
+const callAPI = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        apiData.innerText = JSON.stringify(data);
+    })
+    .catch(e => console.error(new Error(e)));
+}
+
+apiButton.addEventListener('click', callAPI);
+
+
+
